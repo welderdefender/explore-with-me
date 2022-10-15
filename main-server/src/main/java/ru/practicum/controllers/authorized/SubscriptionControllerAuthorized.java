957fc5313a3f8174a372ee36f55a4152df6b89a7
welderdefender <welderdefender@yandex.ru>
2022-10-15 21:45:34 +0300

    Explore With Me - App for posting and finding events - Fixing due to peer review

--- main-server/src/main/java/ru/practicum/controllers/authorized/SubscriptionControllerAuthorized.java
+++ main-server/src/main/java/ru/practicum/controllers/authorized/SubscriptionControllerAuthorized.java
@@ -22,21 +22,25 @@ public class SubscriptionControllerAuthorized {
     @PostMapping(value = "/user/{userId}/friend/{friendId}")
     public SubscriptionDto create(@PathVariable @Positive  long userId,
                                   @PathVariable @Positive long friendId) {
+        log.info("Пользователь с id={} добавил в друзья пользователя с id={}", userId, friendId);
         return subscriptionService.create(userId, friendId);
     }
 
     @DeleteMapping(value = "/{subscriptionId}")
     public void delete(@PathVariable @Positive long subscriptionId) {
+        log.info("Подписка с id={} удалена", subscriptionId);
         subscriptionService.delete(subscriptionId);
     }
 
     @GetMapping(value = "/user/{userId}/events")
     public List<EventShortDto> getEvents(@PathVariable @Positive long userId) {
+        log.info("Получение списка событий у пользователя с id={}", userId);
         return subscriptionService.getEvents(userId);
     }
 
     @GetMapping(value = "/user/{userId}")
     public List<SubscriptionDto> getSubscriptions(@PathVariable @Positive long userId) {
+        log.info("Получение подписок у пользователя с id={}", userId);
         return subscriptionService.getSubscriptions(userId);
     }
 }
